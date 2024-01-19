5a096894cb36ac9629b16052023887aa04be7d16
agusnarvaez <agus.narvaez@outlook.com>
2024-01-19 02:46:51 -0300

    Agrego props y reload a searchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
 import './searchBar.css'
 
-export default function Header() {
-    const [searchValue, setSearchValue] = useState('')
+interface Props{
+  searchValue: string,
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>
+  setReload: React.Dispatch<React.SetStateAction<boolean>>
+  reload: boolean
+}
 
+export const SearchBar:React.FC<Props> = ({searchValue,setSearchValue,setReload,reload}) => {
     const handleSearch = (e:React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
       console.log('search',searchValue)
+      setReload(!reload)
     }
     const handleClearFilters = (e:React.FormEvent<HTMLButtonElement>) => {
       console.log('clear filters',e)
       setSearchValue('')
+      setReload(!reload)
     }
     return (
       <section className='searchBar-section'>
         <form className="filters-container" onSubmit={handleSearch}>
           <section className="searchBar">
-            {/* <input type="text" placeholder="Buscar" id='searchBar-input' className="searchBar-input" /> */}
             <input
               type="text"
               placeholder="Buscar"
