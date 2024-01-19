b5f88a30fbca77b3a146df9999a6235b4bd39633
agusnarvaez <agus.narvaez@outlook.com>
2024-01-19 02:47:47 -0300

    Agrego página de clientes

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,10 +1,53 @@
+import { useEffect, useState } from "react"
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+import {SearchBar } from "../components/SearchBar/SearchBar"
+import { ListOfCLients, emptyClient } from "../models/client"
+import { clientsService } from "../services/clients"
+import { ClientForm } from "../components/ClientForm/ClientForm"
+
+export interface Notification {
+  message:string
+  type:string
+}
+const emptyNotification:Notification = {
+  message:"",
+  type:""
+}
 
 export default function Clients() {
+    const [clients, setClients] = useState<ListOfCLients>([])
+    const [searchValue, setSearchValue] = useState('')
+    const [notification, setNotification] = useState(emptyNotification)
+    const [reload, setReload] = useState(false)
+    useEffect(() => {
+        const fetchClients = async () => {
+            const response = await clientsService.allInstances(searchValue)
+            setClients(response)
+        }
+        fetchClients()
 
-
+    }, [notification,reload])
     return (
       <>
-        <h1>Clientes</h1>
+        <Header />
+        <main>
+            {
+              notification &&
+              <div className={`notification notification_${notification.type}`}>
+                <p>{notification.message}</p>
+              </div>
+            }
+            <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} reload={reload} setReload={setReload} />
+            <ClientForm  client={emptyClient} edition={false} setNotification={setNotification}/>
+            {
+              clients.length > 0 ?
+              clients.map((client) => <ClientForm key={client.id} client={client} edition setNotification={setNotification} />)
+              :
+              <p>No hay clientes</p>
+            }
+        </main>
+        <Footer />
       </>
     )
   }
