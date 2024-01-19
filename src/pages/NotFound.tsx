31b8b69707378e488d6178152c12b6834993fe91
agusnarvaez <agus.narvaez@outlook.com>
2024-01-19 03:20:35 -0300

    Maqueto página not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,14 @@
-
+import { Link } from 'react-router-dom'
 export default function NotFound() {
 
 
   return (
-    <>
-      <h1>No encontrado</h1>
-    </>
+    <main className="notFoundPage">
+      <section>
+        <h1>404</h1>
+        <p>Tu página no fue encontrada</p>
+        <Link to='/'>Volver a challenge</Link>
+      </section>
+    </main>
   )
 }
